209a4880a3a78fcfe50dca93613a385f1747cdaa
I538956 <luca.mueller01@sap.com>
2022-05-16 11:20:00 +0200

    feat: added expression eval for parantheses

--- compiler/ExpressionEvaluator.java
+++ compiler/ExpressionEvaluator.java
@@ -2,30 +2,40 @@ package compiler;
 
 public class ExpressionEvaluator {
     private Lexer m_lexer;
-    
+
     public ExpressionEvaluator(Lexer lexer) {
         m_lexer = lexer;
     }
-    
+
     public int eval(String val) throws Exception {
         m_lexer.init(val);
         return getQuestionMarkExpr();
     }
-    
+
     int getParantheseExpr() throws Exception {
         Token curToken = m_lexer.lookAhead();
-        m_lexer.expect(Token.Type.INTEGER);
-        return Integer.valueOf(curToken.m_value);
+        int result = 0;
+
+        if (curToken.m_type == Token.Type.LPAREN) {
+            m_lexer.expect(Token.Type.LPAREN);
+            result = getQuestionMarkExpr();
+            m_lexer.expect(Token.Type.RPAREN);
+
+        } else if (curToken.m_type == Token.Type.INTEGER) {
+            result = Integer.valueOf(curToken.m_value);
+        }
+
+        return result;
     }
-    
+
     int getUnaryExpr() throws Exception {
         return getParantheseExpr();
     }
-    
+
     int getMulDivExpr() throws Exception {
         return getUnaryExpr();
     }
-    
+
     int getPlusMinusExpr() throws Exception {
         int result = getMulDivExpr();
         Token nextToken = m_lexer.lookAhead();
@@ -35,7 +45,7 @@ public class ExpressionEvaluator {
                 result += getMulDivExpr();
             } else {
                 m_lexer.advance();
-                result -= getMulDivExpr();                
+                result -= getMulDivExpr();
             }
             nextToken = m_lexer.lookAhead();
         }
